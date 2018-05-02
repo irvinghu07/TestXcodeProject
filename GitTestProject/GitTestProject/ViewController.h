238ef14c2481277e6c1ea9711c47722a964c5957
Irving Hu <rhu5@dons.usfca.edu>
2018-05-02 01:53:47 -0700

    This is the first message send from Xcode

---
+++ GitTestProject/GitTestProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitTestProject
+//
+//  Created by irving on 2018/5/2.
+//  Copyright © 2018年 irving. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitTestProject/GitTestProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GitTestProject
+//
+//  Created by irving on 2018/5/2.
+//  Copyright © 2018年 irving. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
